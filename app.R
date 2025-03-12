ef2be5a22a874ad1983a4814164a23c5145e3829
unknown <ogilmar@protonmail.com>
2025-03-12 15:52:27 +0000

    added shinyWidgets, filter and table, and scaterplot

--- app.R
+++ app.R
@@ -2,6 +2,8 @@
 library(shiny)
 library(tidyverse)
 library(shinydashboard)
+library(shinyWidgets)
+library(DT)
 
 # Load data
 listings <- rio::import("data/listings.csv")
@@ -18,9 +20,31 @@ listings <- listings %>%
 ui <- dashboardPage(
   dashboardHeader(title = "Rio Airbnb Dashboard"),
   dashboardSidebar(
-    sliderInput("bins", "Number of bins:", min = 5, max = 50, value = 25),
-    selectInput("color", "Choose a color:", choices = c("Green" = "#00c244", "Blue" = "#007bc2")),
-    selectInput("theme", "Choose a theme:", choices = c("Classic", "Minimal", "Dark"))
+    sliderInput("bins", 
+                "Number of bins:",
+                min = 5, 
+                max = 50, 
+                value = 25),
+    pickerInput("color", 
+                "Choose a color:",
+                choices = c("Green" = "#00c244", "Blue" = "#007bc2", "Red" = "#c20000"),
+                selected = "#00c244",
+                multiple = FALSE,
+                options = list(style = "btn-primary")),
+    pickerInput("theme", 
+                "Choose a theme:", 
+                choices = c("Classic", "Minimal", "Dark"),
+                selected = "Dark"),
+    pickerInput(
+                inputId = "neighbourhood_filter", 
+                label = "Select the neigghbourhood:", 
+                choices = unique(listings$neighbourhood), 
+                options = pickerOptions(
+                actionsBox = TRUE, 
+                size = 10,
+                selectedTextFormat = "count > 3"), 
+                multiple = TRUE
+    )
   ),
   dashboardBody(
     fluidRow(
@@ -68,10 +92,29 @@ ui <- dashboardPage(
         collapsible = TRUE,
         plotOutput("boxPlot")
       )
+    ),
+    fluidRow(
+      box(
+        width = 12,
+        title = "Price vs availability on next 365 days",
+        status = "primary",
+        solidHeader = TRUE,
+        collapsible = TRUE,
+        plotOutput("scatterplot")
+      )
+    ),
+    fluidRow(
+      box(
+        width = 12,
+        title = "Airbnb listings at Rio de Janeiro",
+        status = "primary",
+        solidHeader = TRUE,
+        collapsible = TRUE,
+        DT::DTOutput("listingsTable")
+      )
     )
-  )
 )
-
+)
 
 # Define server logic -----------------------------------------------------
 server <- function(input, output) {
@@ -107,7 +150,7 @@ server <- function(input, output) {
    
    output$densityPlot <- renderPlot({
      x <- listings$price
-     
+
      ggplot(data.frame(x), aes(x)) +
        geom_density(fill = input$color, alpha = 0.5) +
        labs(title = "Density Plot of Prices, in Brazilian Reais (R$)", x = "Prices (R$)", y = "Density") +
@@ -121,6 +164,34 @@ server <- function(input, output) {
        geom_boxplot(fill = input$color, color = "black") +
        labs(title = "Boxplot of Prices, in Brazilian Reais (R$)", y = "Prices (R$)") +
        theme_choice()
+   })
+   
+   output$scatterplot <- renderPlot({
+     x <- listings$price
+     y <- listings$availability_365
+     
+     ggplot(data.frame(x, y)) +
+       aes(x = x, y = y) +
+       geom_point() +
+       labs(x = "Price", y = "Availability on the next 365 days", title = "Price vs Availability") + 
+       theme_minimal()  # Adiciona um tema para melhor visualização
+   })
+
+   filtered_listings <- reactive({
+     if (length(input$neighbourhood_filter) == 0) {
+       listings
+     } else {
+       listings %>% 
+         filter(neighbourhood %in% input$neighbourhood_filter)
+     }
+   })
+   output$listingsTable <- DT::renderDataTable({
+     DT::datatable(
+       filtered_listings() %>%
+         select(name, neighbourhood, room_type, price, number_of_reviews, availability_365, number_of_reviews_ltm, calculated_host_listings_count),
+       options = list(pageLength = 10, autoWidth = TRUE),
+       rownames = FALSE
+     )
    })
  }
 
